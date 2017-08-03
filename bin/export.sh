947c52cd6173bffbbe2a3fa1195a318d37a34f28
Scott Wang <scott@malloc.sh>
2017-08-03 08:47:07 -0700

    simpfy the tool

--- bin/export.sh
+++ bin/export.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -jar target/dynamodb-import-export-tool-1.0.1.jar --destinationEndpoint dynamodb.$3.amazonaws.com --destinationTable $4 --sourceEndpoint dynamodb.$1.amazonaws.com --sourceTable $2 --readThroughputRatio 0.25 --writeThroughputRatio 0.25
+java -jar target/dynamodb-import-export-tool-1.0.1.jar --destinationEndpoint dynamodb.$3.amazonaws.com --destinationTable $1 --sourceEndpoint dynamodb.$2.amazonaws.com --sourceTable $1 --readThroughputRatio 0.25 --writeThroughputRatio 0.25
